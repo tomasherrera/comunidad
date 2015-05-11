95f48ee882170d76d270b9324bdffeff17a67f4c
Tomas Herrera <tomasherrera91@hotmail.com>
2015-05-11 02:32:35 -0500

    removed confirmable module

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+config.action_mailer.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {
+  :address  => "smtp.sendgrid.net",
+  :port  => 25,
+  :user_name  => "app36474527@heroku.com",
+  :password  => "mypass",
+  :authentication  => :login
+}
